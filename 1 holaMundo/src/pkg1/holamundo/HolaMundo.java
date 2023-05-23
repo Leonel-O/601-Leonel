32a168c993f1db992536354d8dacccabdf06460f
unknown <leonel.ortiz.solis@upvm.edu.mx>
2023-05-23 17:09:47 -0500

    Se agrega Hola mundo

---
+++ 1 holaMundo/src/pkg1/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+
+package pkg1.holamundo;
+
+
+public class HolaMundo {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+    }
+    
+}
